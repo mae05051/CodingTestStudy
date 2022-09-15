d8f1c3b7cd6bad5b4eb6317a3163dd8d91e377bb
hans <mae054954@naver.com>
2022-09-15 19:55:50 +0900

    1

--- 1주차_Hash/고득점 kit - 전화번호 목록 (2단계).py
+++ 1주차_Hash/고득점 kit - 전화번호 목록 (2단계).py
@@ -1,38 +1,15 @@
 def solution(phone_book):
-    #띄어쓰기 제거, str -> int, sort
-    # for st in range(len(phone_book)):
-    #     phone_book[st]=int(phone_book[st].replace(' ',''))
-    phone_book.sort()
-    #print(phone_book)
-    #다시 int -> str
-    # for it in range(len(phone_book)):
-    #     phone_book[it]=str(phone_book[it])
-    #print(phone_book)
+    phone_book.sort() #사전식 정렬
 
     for pb in range(len(phone_book)):
-
-        breaker1=''
-        breaker2=''
-
-        if phone_book[pb]==phone_book[-1]:
+        
+        if phone_book[pb]==phone_book[-1]:#제일 마지막 요소일 때 -> 끝까지 검사해서 접두어 관계가 없었기 때문에 True
             return True
 
-        if len(phone_book[pb])>=len(phone_book[pb+1]):
-            continue
-
-        for s in phone_book[pb+1:]:
-            if s.find(phone_book[pb])==0:
-                breaker1='breaker1'
-                break
-            if len(phone_book[pb])>=len(s):
-                breaker2='breaker2'
-        
-        if breaker1=='breaker1':
+        #사전식으로 정렬했기 때문에 인덱스 처음부터 두개씩 비교 -> pb번째가 pb+1번째의 접두어가 아니면 그 뒤의 요소들을 검사할 필요가 없음
+        if phone_book[pb+1].startswith(phone_book[pb]): 
             return False
 
-        if breaker2=='breaker2':
-            continue
-        
 print(solution(["119", "97674223", "1195524421"]))
 print(solution(["123","456","789"]))
 print(solution(["12","123","1235","567","88"]))
